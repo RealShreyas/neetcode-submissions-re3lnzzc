07e2725b822fedb16b096cc1b12e7884630cd045
Shreyas More <shrmore@gmail.com>
2026-07-26 19:54:24 +0530

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 20
+print(type(variable))
+variable = 20.20
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "YASHAS"
+print(type(variable))
+variable = [1.2,2.3,3.4]
+print(type(variable))
