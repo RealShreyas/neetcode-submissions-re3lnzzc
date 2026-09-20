3154d2741f7605f1da6a1ce4a36e8cb5ae86aa2b
Shreyas More <shrmore@gmail.com>
2026-09-20 16:14:40 +0530

    Add: python-variable-types - submission-2

---
+++ Python For Beginners/python-variable-types/submission-2.py
@@ -0,0 +1,10 @@
+integer_type =20
+float_type = 20.20
+bool_type = True
+string_type = "YASHAS"
+list_type = [1,2,3,4,5]
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
